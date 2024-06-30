d0d1c40019e646d32cc0fee197c94d69e37dc38c
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-06-30 21:13:10 +0530

    Remove unused elements of module

--- frontend/nistara/modules/nearby-connections-expo/android/src/main/java/expo/modules/nearbyconnectionsexpo/NearbyConnectionsExpoModule.kt
+++ frontend/nistara/modules/nearby-connections-expo/android/src/main/java/expo/modules/nearbyconnectionsexpo/NearbyConnectionsExpoModule.kt
@@ -13,35 +13,5 @@ class NearbyConnectionsExpoModule : Module() {
     // The module will be accessible from `requireNativeModule('NearbyConnectionsExpo')` in JavaScript.
     Name("NearbyConnectionsExpo")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(NearbyConnectionsExpoView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: NearbyConnectionsExpoView, prop: String ->
-        println(prop)
-      }
-    }
   }
 }
