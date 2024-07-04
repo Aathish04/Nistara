ca1b75c365b0c8df956c22a45a149c38ab3a25db
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-07-04 12:21:34 +0530

    Import hello from nearby-connections-expo module

--- frontend/nistara/App.tsx
+++ frontend/nistara/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { hello } from './modules/nearby-connections-expo';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{hello()}</Text>
       <StatusBar style="auto" />
     </View>
   );
