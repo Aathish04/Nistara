5fb9d7d13643a1a41eeec68b50d9793204607dc5
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-07-05 16:00:43 +0530

    Copy over Google's NearbyConnections's Sample from https://github.com/android/connectivity-samples/blob/main/UwbRanging/uwbranging/src/main/java/com/google/apps/uwbranging/impl/NearbyConnections.kt

--- frontend/nistara/modules/nearby-connections-expo/android/src/main/java/expo/modules/nearbyconnectionsexpo/NearbyConnections.kt
+++ frontend/nistara/modules/nearby-connections-expo/android/src/main/java/expo/modules/nearbyconnectionsexpo/NearbyConnections.kt
@@ -27,5 +27,142 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 
-// private const val CONNECTION_SERVICE_ID = "org.nistara.nistara"
-// private const val CONNECTION_NAME = "org.nistara.nistara"
+private const val CONNECTION_SERVICE_ID = "com.nistara.nistara"
+private const val CONNECTION_NAME = "com.nistara.nistara"
+
+/**
+ * Nearby Connections API wrapper.
+ *
+ * @param name Human readable name used in Nearby Connections.
+ * @param dispatcher Dispatcher that runs Nearby Connections API calls.
+ * @param connectionsClient a Nearby Connections client. Exposed for unit testing.
+ */
+internal class NearbyConnections(
+  context: Context,
+  private val dispatcher: CoroutineDispatcher,
+  private val connectionsClient: ConnectionsClient = Nearby.getConnectionsClient(context),
+) {
+
+  private val coroutineScope =
+    CoroutineScope(
+      dispatcher +
+        Job() +
+        CoroutineExceptionHandler { _, e -> Log.e("NearbyConnections", "Connection Error", e) }
+    )
+
+  // Connection-phase Callbacks used by both controller and controlee
+  private val connectionLifecycleCallback =
+    object : ConnectionLifecycleCallback() {
+      override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+        coroutineScope.launch {
+          connectionsClient.acceptConnection(endpointId, payloadCallback).await()
+        }
+      }
+
+      override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+        if (result.status.statusCode == ConnectionsStatusCodes.STATUS_OK) {
+          dispatchEvent(NearbyEvent.EndpointConnected(endpointId))
+        }
+      }
+
+      override fun onDisconnected(endpointId: String) {
+          dispatchEvent(NearbyEvent.EndpointLost(endpointId))
+      }
+    }
+
+  private val payloadCallback =
+    object : PayloadCallback() {
+      override fun onPayloadReceived(endpointId: String, payload: Payload) {
+        val bytes = payload.asBytes() ?: return
+        dispatchEvent(NearbyEvent.PayloadReceived(endpointId, bytes))
+      }
+
+      override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {}
+    }
+
+  private val endpointDiscoveryCallback =
+    object : EndpointDiscoveryCallback() {
+      override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+        coroutineScope.launch {
+          connectionsClient
+            .requestConnection(CONNECTION_NAME, endpointId, connectionLifecycleCallback)
+        }
+      }
+
+      override fun onEndpointLost(endpointId: String) {
+        dispatchEvent(NearbyEvent.EndpointLost(endpointId))
+
+      }
+    }
+
+  fun sendPayload(endpointId: String, bytes: ByteArray) {
+    coroutineScope.launch {
+      connectionsClient.sendPayload(endpointId, Payload.fromBytes(bytes)).await()
+    }
+  }
+
+  private var dispatchEvent: (event: NearbyEvent) -> Unit = {}
+
+  /**
+   * Starts discovery.
+   * @return a flow of [NearbyEvent].
+   */
+  fun startDiscovery() = callbackFlow {
+    dispatchEvent = { trySend(it) }
+    coroutineScope.launch {
+      connectionsClient
+        .startDiscovery(
+          CONNECTION_SERVICE_ID,
+          endpointDiscoveryCallback,
+          DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build()
+        )
+        .await()
+    }
+    awaitClose {
+      disconnectAll()
+      connectionsClient.stopDiscovery()
+    }
+  }
+
+  /**
+   * Starts advertising.
+   * @return a flow of [NearbyEvent].
+   */
+  fun startAdvertising() = callbackFlow {
+    dispatchEvent = { trySend(it) }
+    coroutineScope.launch {
+      connectionsClient
+        .startAdvertising(
+          CONNECTION_NAME,
+          CONNECTION_SERVICE_ID,
+          connectionLifecycleCallback,
+          AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build()
+        )
+        .await()
+    }
+    awaitClose {
+      disconnectAll()
+      connectionsClient.stopAdvertising()
+    }
+  }
+
+  private fun disconnectAll() {
+    connectionsClient.stopAllEndpoints()
+  }
+}
+
+/** Events that happen in a Nearby Connections session. */
+abstract class NearbyEvent private constructor() {
+
+  abstract val endpointId: String
+
+  /** An event that notifies a NC endpoint is connected. */
+  data class EndpointConnected(override val endpointId: String) : NearbyEvent()
+
+  /** An event that notifies a NC endpoint is lost. */
+  data class EndpointLost(override val endpointId: String) : NearbyEvent()
+
+  /** An event that notifies a UWB device is lost. */
+  data class PayloadReceived(override val endpointId: String, val payload: ByteArray) :
+    NearbyEvent()
+}
