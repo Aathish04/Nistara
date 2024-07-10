5e1ffbdb012b6502869a6e9435f0e98adbb0a437
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-07-10 20:06:42 +0530

    Listen to events

--- frontend/nistara/App.tsx
+++ frontend/nistara/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { startAdvertising, startDiscovery, sanitycheck,requestPermissionsAsync} from './modules/nearby-connections-expo';
+import { startAdvertising, startDiscovery, sanitycheck,requestPermissionsAsync, addOnEndpointConnectedListener, addOnEndpointLostListener} from './modules/nearby-connections-expo';
 export default function App() {
   let [perms,setPerms] = useState("No Perms Yet")
   async function requestPerms(){
     setPerms(JSON.stringify(await requestPermissionsAsync()))
   }
+
+  addOnEndpointConnectedListener((event)=>{console.log("Endpoint Found:"+JSON.stringify(event))})
+  addOnEndpointLostListener((event)=>{console.log("Endpoint Lost: "+JSON.stringify(event))})
   return (
     <View style={styles.container}>
       <Text>{sanitycheck()}</Text>
