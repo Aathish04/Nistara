34be143be5424c27b9f95bf5667ff7cb3bebc3ab
Jahnavi Murali <109456718+jahnavimurali@users.noreply.github.com>
2024-07-24 19:03:21 +0530

    sos screen complete

---
+++ frontend/nistara/newsdata/soshelplines.tsx
@@ -0,0 +1,38 @@
+export const sosHelplines = [
+    {
+       title: "National Emergency Helpline", number: "112",
+    },
+    {
+        title: "Police", number: "100"
+    },
+    {
+        title: "FireRescue", number: "101"
+    },
+    {
+        title: "Ambulance", number: "108"
+    },
+    {
+        title: "Disaster Management (NDMA)", number: "1078"
+    },
+    {
+        title: "Gas Leak Helpline", number: "1906"
+    },
+    {
+        title: "Emergency Child Care", number: "1098"
+    },
+    {
+        title: "Relief Commissioner for Natural Calamities", number: "1070"
+    },
+    {
+        title: "Earthquake/ Flood/ Disaster (NDRF HQ)", number: "011-24363260"
+    },
+    {
+        title: "Senior Citizen Helpline", number: "14567"
+    },
+    {
+        title: "Road Accident Emergency Services", number: "1073"
+    },
+    { 
+        title: 'Railway Accident Emergency Service', number: '1072'
+    }
+]
